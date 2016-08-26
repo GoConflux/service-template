983b0639307998c200df38629bce216b32fc78d6
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-08-25 20:41:09 -0700

    sending back valid url this time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   def validate_creds
     auth = Rack::Auth::Basic::Request.new(request.env)
 
-    puts auth.provided?
-    puts auth.basic?
-    puts auth.credentials
-    puts [ENV['CONFLUX_USERNAME'], ENV['CONFLUX_PASSWORD']]
-
     if !auth.provided? || !auth.basic? || (auth.credentials != [ENV['CONFLUX_USERNAME'], ENV['CONFLUX_PASSWORD']])
       render json: { message: 'Invalid credentials' }, status: 401
     end
